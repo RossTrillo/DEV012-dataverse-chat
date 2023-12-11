7addb465b73cf272eb9e8a90aad73506d2f2f471
Rosario <rosario.trillo@unmsm.edu.pe>
2023-12-11 17:47:33 -0500

    Creating remaining views

--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
 import home from "./viewsComponents/homeView/home.js";
 import { errorView } from "./viewsComponents/errorView/errorView.js";
 import {apiView} from "./viewsComponents/apiView/apiView.js";
+import { groupChatView } from "./viewsComponents/groupChat/groupChat.js";
+import { singleChatView } from "./viewsComponents/singleChat/chatView.js";
 
 const routes = {
   "/": home,
   "/404": errorView,
   "/api": apiView,
+  "/group": groupChatView,
+  "/chat" : singleChatView
 };
 
 function render(template) {
