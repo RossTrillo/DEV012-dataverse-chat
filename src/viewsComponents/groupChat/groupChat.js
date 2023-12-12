a1cc0b4178064782db06d1e10755e1c3591e9bc6
Rosario <rosario.trillo@unmsm.edu.pe>
2023-12-12 17:31:25 -0500

    Creating SingleChat

--- src/viewsComponents/groupChat/groupChat.js
+++ src/viewsComponents/groupChat/groupChat.js
@@ -1,6 +1,6 @@
 import { footer } from "../../staticsComponents/footer.js";
 export const groupChatView = () => {
-
+    
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = './viewsComponents/groupChat/stylesGroup.css';
@@ -8,7 +8,7 @@ export const groupChatView = () => {
     document.head.appendChild(link);
 
     const container = document.createElement('div')
-    container.setAttribute("id", "groupView")
+    container.setAttribute("id", "chatView")
 
     const groupHeader = document.createElement("header")
     groupHeader.setAttribute("class", "header")
@@ -19,26 +19,41 @@ export const groupChatView = () => {
     </a>
     `
 
-    const groupRoot = document.createElement('main')
-    groupRoot.setAttribute("id", "apiRoot")
-    groupRoot.setAttribute("class", "main")
-    groupRoot.innerHTML = `
-    <section id ="chat">
-    "Chat grupal"
-    </section>
-    <section id ="questions">
+    const groupChatSection = document.createElement('section')
+    groupChatSection.setAttribute("id", "singleSection")
+    groupChatSection.innerHTML = `
+    <aside class="cardInfo">
+    "Información de cada programa"
+    </aside>
+    <main class ="singleChat">
+    </main>
+    `
+
+    const inputText = document.createElement('section')
+    inputText.setAttribute("id", "inputText")
+    inputText.innerHTML = ` <section id="questions">
     <label for="text" id="phrase"> Un momento, están pensando...</label>
     <br>
-    <input id ="text" type="text" >
-    <a href="/">
-    <button type="submit" id="button">Enviar</button>
-    </a>
-    <br>
+    <input id ="text" type="text"  placeholder="Escribe tu pregunta...">
     </section>
+    <button type="submit" id="button">Enviar</button>
     `
+
+
     container.appendChild(groupHeader);
-    container.appendChild(groupRoot);
+    container.appendChild(groupChatSection);
+    container.appendChild(inputText);
     document.body.appendChild(container);
     document.body.appendChild(footer);
-    return container
-}
+
+
+
+    const script = document.createElement("script");
+    script.src= "./viewsComponents/groupChat/mainGroup.js"
+    script.type ="module"
+    document.body.appendChild(script); 
+
+    return container;
+
+
+};
--- src/viewsComponents/singleChat/mainSingleChat.js
+++ src/viewsComponents/singleChat/mainSingleChat.js
@@ -1,3 +1,39 @@
+/*
+const interactionWithIa = document.getElementById("button");
+interactionWithIa.addEventListener("click", addEventListeners); //
+
+
+
+function addEventListeners () {
+function sendMessage () {
+    const messageInput = document.getElementById("text"); //
+    
+    const message = messageInput.value;//
+
+
+    addMessage("Usuari@", message);
+
+    const answersIA = getAnswerIA(message);
+
+    addMessage("Personaje", answersIA);
+
+    messageInput.value = "";
+
+    }
+function addMessage(sender, message) {
+    const mainContainer = document.querySelector(".singleChat");
+    const newMessage = document.createElement("p");
+    newMessage.innerHTML = `${sender}: ${message}`;
+    mainContainer.appendChild(newMessage);
+}
+
+function getAnswerIA(message) {
+    return message;
+}
+}
+console.log(addEventListeners());
+*/
+
 const interactionWithIa = document.getElementById("button");
 interactionWithIa.addEventListener("click", sendMessage); //
 
@@ -16,7 +52,7 @@ interactionWithIa.addEventListener("click", sendMessage); //
 
         const answersIA = getAnswerIA(message);
 
-        addMessage("Personajes", answersIA);
+        addMessage("Personaje", answersIA);
 
         messageInput.value = "";
     }
